82497e0a984dfee8c3ce0d8ad3ca31f019e9abc5
Will <wjeckert15@gmail.com>
2020-11-19 16:22:59 -0600

    i dont know what this is

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,4 +1,4 @@
-import store from "./store.js"
+import store from "./store.js";
 
 const generateItemElement = function (item) {
   let itemTitle = `<span class="shopping-item shopping-item__checked">${item.name}</span>`;
