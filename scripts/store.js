a3f32bad4d7190a1a868e2bddba5e2d1f91da789
Will <wjeckert15@gmail.com>
2020-12-01 15:29:48 -0600

    pushing stuff

--- scripts/store.js
+++ scripts/store.js
@@ -2,10 +2,7 @@
 
 const store = {
     items: [
-        { id: cuid(), name: 'apples', checked: false },
-        { id: cuid(), name: 'oranges', checked: false },
-        { id: cuid(), name: 'milk', checked: true },
-        { id: cuid(), name: 'bread', checked: false }
+        
       ],
       hideCheckedItems: false
     }
